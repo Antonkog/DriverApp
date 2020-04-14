88ac75f803c66d39ecbc0fb441e962c8980a9543
Murat Kös <mkoes@hotmail.de>
2020-04-14 14:14:29 +0200

    Ver 1.1.8 - Configuration for Release-Signing

--- buildSrc/src/main/java/dependencies/Versions.kt
+++ buildSrc/src/main/java/dependencies/Versions.kt
@@ -6,7 +6,7 @@ private object Versions {
 
     private val versionMajor = 1
     private val versionMinor = 1    // Release gerade Zahlen im PlayStore
-    private val versionPatch = 7
+    private val versionPatch = 8
     private val versionOffset = 0
     val androidVersionCode = (versionMajor * 10000 + versionMinor * 100 + versionPatch) * 100 + versionOffset
 
