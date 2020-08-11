4ff6b4fa28aa61d6c893a67e3f5514e6ba3574d2
Anton Kogan <akogan777@gmail.com>
2020-08-11 12:32:09 +0300

    parsing json to show all fields fast.

---
+++ app/src/main/java/com/redhotapp/driverapp/ui/home/JsonParser.kt
@@ -0,0 +1,50 @@
+package com.redhotapp.driverapp.ui.home
+
+import android.util.Log
+import org.json.JSONArray
+import org.json.JSONException
+import org.json.JSONObject
+import java.util.*
+
+object JsonParser {
+
+    val TAG = "JsonParser"
+    /**
+     * parse json object
+     * @param objJson
+     * @return  Map<String, String>
+     * @throws JSONException
+     */
+    @Throws(JSONException::class)
+    fun parseJson(objJson: Any?): Map<String, String> {
+        val map = HashMap<String, String>()
+
+        // If obj is a json array
+        if (objJson is JSONArray) {
+            for (i in 0 until objJson.length()) {
+                parseJson(objJson[i])
+            }
+        } else if (objJson is JSONObject) {
+            val it: Iterator<*> = objJson.keys()
+            while (it.hasNext()) {
+                val key = it.next().toString()
+                // If you get an array
+                when (val jobject = objJson[key]) {
+                    is JSONArray -> {
+                        Log.e(TAG, " JSONArray: $jobject")
+                        parseJson(jobject)
+                    }
+                    is JSONObject -> {
+                        Log.e(TAG, " JSONObject: $jobject")
+                        parseJson(jobject)
+                    }
+                    else -> {
+                        Log.e(TAG, " adding to map: $key $jobject")
+                        map[key] = jobject.toString()
+                    }
+                }
+            }
+        }
+        return map
+    }
+}
--- app/src/main/java/com/redhotapp/driverapp/ui/home/TasksAdapter.kt
+++ app/src/main/java/com/redhotapp/driverapp/ui/home/TasksAdapter.kt
@@ -1,18 +1,76 @@
 package com.redhotapp.driverapp.ui.home
 
-import android.net.nsd.NsdServiceInfo
-import androidx.core.content.res.ResourcesCompat
+import android.view.LayoutInflater
+import android.widget.LinearLayout
+import android.widget.TextView
+import com.google.gson.Gson
+import com.google.gson.JsonElement
 import com.kivi.remote.presentation.base.recycler.LazyAdapter
 import com.redhotapp.driverapp.R
 import com.redhotapp.driverapp.data.model.AllTask
-import com.redhotapp.driverapp.data.model.abona.TaskItem
 import com.redhotapp.driverapp.databinding.TaskItemBinding
+import org.json.JSONException
+import org.json.JSONObject
+
 
 class TasksAdapter(itemClickListener: HomeFragment) : LazyAdapter<AllTask, TaskItemBinding>(itemClickListener) {
 
     override fun bindData(data: AllTask, binding: TaskItemBinding) {
         binding.root.setOnClickListener { itemClickListener?.onLazyItemClick(data) }
-        binding.taskText.text = "task: $data"
+        binding.progressBar.progress = if(data.status > 100) 100 else (data.status)   //text = "task: $data"
+
+        val jsonObject = JSONObject(Gson().toJson(data).trim())
+        val map: Map<String, String> = JsonParser.parseJson(jsonObject)
+
+        val linearContent : LinearLayout = binding.linlayDescription
+
+        map.entries.forEach { entry ->
+            run {
+                val row = LayoutInflater.from(binding.root.context)
+                    .inflate(R.layout.task_row, null, false)
+                row.findViewById<TextView>(R.id.txt_item_row).text = entry.key + " " + entry.value
+                linearContent.addView(row)
+            }
+        }
+
+
+
+/*
+LayoutInflater vi = (LayoutInflater) getApplicationContext().getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+View v = vi.inflate(R.layout.your_layout, null);
+
+// fill in any details dynamically here
+TextView textView = (TextView) v.findViewById(R.id.a_text_view);
+textView.setText("your text");
+
+// insert into main view
+ViewGroup insertPoint = (ViewGroup) findViewById(R.id.insert_point);
+insertPoint.addView(v, 0, new ViewGroup.LayoutParams(ViewGroup.LayoutParams.FILL_PARENT, ViewGroup.LayoutParams.FILL_PARENT));
+ */
+
+//        "MandantId":3,
+//        "MandantName":"Hegelmann Express GmbH",
+//        "TaskId":1213,
+//        "TaskChangeId":2331,
+//        "AbonaTransferNr":"",
+//        "PreviousTaskId":0,
+//        "NextTaskId":1215,
+//        "VehiclePreviousTaskId":0,
+//        "VehicleNextTaskId":0,
+//        "ChangeReason":1,
+//        "ActionType":0,
+//        "OrderNo":202023123,
+//        "Description":null,
+//        "KundenName":"Amazon",
+//        "KundenNr":30118,
+//        "ReferenceIdCustomer1":"a",
+//        "ReferenceIdCustomer2":null,
+//        "PalletsAmount":0,
+//        "TaskDueDateStart":"0001-01-01T00:00:00",
+//        "TaskDueDateFinish":"2020-06-03T00:00:00",
+//        "Status":100,
+//        "PercentFinishedActivities":0,
+//
     }
 
     override fun getLayoutId(): Int = R.layout.task_item
