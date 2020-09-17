c300d923c37cb2b6e9fce258a7e0f1eab43d3dbf
Anton Kogan <akogan777@gmail.com>
2020-09-17 16:17:36 +0300

    Change button to tabs (Task#7739).

--- src/DriverApp/app/src/main/java/com/abona_erp/driver/app/ui/feature/main/fragment/history/HistoryFragment.java
+++ src/DriverApp/app/src/main/java/com/abona_erp/driver/app/ui/feature/main/fragment/history/HistoryFragment.java
@@ -18,6 +18,8 @@ import com.abona_erp.driver.app.BuildConfig;
 import com.abona_erp.driver.app.R;
 import com.abona_erp.driver.app.ui.event.PageEvent;
 import com.abona_erp.driver.app.ui.feature.main.PageItemDescriptor;
+import com.abona_erp.driver.app.util.AppUtils;
+import com.google.android.material.tabs.TabLayout;
 
 public class HistoryFragment extends Fragment {
 
@@ -27,8 +29,10 @@ public class HistoryFragment extends Fragment {
   private RecyclerView recyclerView;
   private HistoryAdapter historyAdapter;
   private RecyclerView.LayoutManager layoutManager;
-  private AppCompatButton btnClearLog, btnOrderNo, btnTaskId;
+  private AppCompatButton btnClearLog;
   private int currentTaskId = 0, currentOrderNo = 0;
+  private TabLayout tabLayout;
+  private final int tabTaskPosition = 0, tabOrdersPosition = 1;
   public HistoryFragment() {
     // Required empty public constructor.
   }
@@ -71,6 +75,37 @@ public class HistoryFragment extends Fragment {
     historyViewModel.getHistoryLogs().observe(getViewLifecycleOwner(), logItems -> historyViewModel.refreshLogs(logItems, currentTaskId, currentOrderNo));
     historyViewModel.getFilteredLogs().observe(getViewLifecycleOwner(), logItems -> historyAdapter.swapData(logItems));
 
+
+    tabLayout = root.findViewById(R.id.tabs);
+    if (currentTaskId != 0)
+      tabLayout.getTabAt(tabTaskPosition).setText(currentTaskId + "");
+    if (currentOrderNo != 0)
+      tabLayout.getTabAt(tabOrdersPosition).setText(AppUtils.parseOrderNo(currentOrderNo));
+
+    tabLayout.addOnTabSelectedListener(new TabLayout.OnTabSelectedListener() {
+      @Override
+      public void onTabSelected(TabLayout.Tab tab) {
+        switch (tab.getPosition()){
+          case tabTaskPosition:
+            refreshByTaskId();
+            break;
+          case tabOrdersPosition:
+            refreshByOrderNo();
+            break;
+        }
+      }
+
+      @Override
+      public void onTabUnselected(TabLayout.Tab tab) {
+
+      }
+
+      @Override
+      public void onTabReselected(TabLayout.Tab tab) {
+
+      }
+    });
+
     if (BuildConfig.DEBUG) {
       btnClearLog = root.findViewById(R.id.btn_clear_log);
       btnClearLog.setOnClickListener(v -> historyViewModel.deleteChangeHistory());
@@ -78,16 +113,6 @@ public class HistoryFragment extends Fragment {
     } else {
       btnClearLog.setVisibility(View.GONE);
     }
-
-    btnOrderNo = root.findViewById(R.id.btn_order_no);
-    btnOrderNo.setOnClickListener(v -> {
-      refreshByOrderNo();
-    });
-
-    btnTaskId = root.findViewById(R.id.btn_task_id);
-    btnTaskId.setOnClickListener(v -> {
-      refreshByTaskId();
-    });
   }
 
   private void refreshByOrderNo() {
