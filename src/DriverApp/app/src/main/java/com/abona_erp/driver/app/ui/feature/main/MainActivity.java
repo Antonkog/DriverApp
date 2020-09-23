88408a76b8c105fd81e854744c031feb5d1c38e5
Anton Kogan <akogan777@gmail.com>
2020-09-23 15:28:04 +0300

    BugFix: removed not existing import, onBackPress in PhotoFragment (Task#7820).

--- src/DriverApp/app/src/main/java/com/abona_erp/driver/app/ui/feature/main/MainActivity.java
+++ src/DriverApp/app/src/main/java/com/abona_erp/driver/app/ui/feature/main/MainActivity.java
@@ -61,7 +61,6 @@ import com.abona_erp.driver.app.receiver.LocaleChangeReceiver;
 import com.abona_erp.driver.app.receiver.NetworkChangeReceiver;
 import com.abona_erp.driver.app.service.BackgroundServiceWorker;
 import com.abona_erp.driver.app.service.ForegroundAlarmService;
-import com.abona_erp.driver.app.ui.base.BaseActivity;
 import com.abona_erp.driver.app.ui.event.ChangeHistoryEvent;
 import com.abona_erp.driver.app.ui.event.ConnectivityEvent;
 import com.abona_erp.driver.app.ui.event.DocumentEvent;
@@ -210,9 +209,10 @@ public class MainActivity extends BaseActivity /*implements OnCompleteListener<V
 
   @Override
   public void onBackPressed() {
-    if(getSupportFragmentManager().getBackStackEntryCount() > 1 )
+    if(getSupportFragmentManager().getBackStackEntryCount() > 1 ){
+      tellFragmentsOnBackPress();
       App.eventBus.post(new PageEvent(new PageItemDescriptor(PageItemDescriptor.PAGE_BACK), null));
-    else {
+    } else {
       Util.askNeedExit(MainActivity.this);
     }
   }
