d5c8423960109ab119a342c1ef524e2c1693decf
Anton Kogan <akogan777@gmail.com>
2020-09-29 11:59:33 +0300

    Glide module to load document previews.

--- app/src/main/java/com/abona_erp/driver/app/ui/documents/DocumentsAdapter.kt
+++ app/src/main/java/com/abona_erp/driver/app/ui/documents/DocumentsAdapter.kt
@@ -4,20 +4,16 @@ import com.kivi.remote.presentation.base.recycler.LazyAdapter
 import com.abona_erp.driver.app.R
 import com.abona_erp.driver.app.data.model.DocumentResponse
 import com.abona_erp.driver.app.databinding.DocumentItemBinding
+import com.bumptech.glide.Glide
+import com.bumptech.glide.load.engine.DiskCacheStrategy
 
 
 class DocumentsAdapter : LazyAdapter<DocumentResponse, DocumentItemBinding>(null) {
 
     override fun bindData(data: DocumentResponse, binding: DocumentItemBinding) {
         binding.root.setOnClickListener { itemClickListener?.onLazyItemClick(data) }
+        Glide.with(binding.root.context).load(data.linkToFile).diskCacheStrategy(DiskCacheStrategy.NONE).into(binding.documentImage)
         binding.documentText.text = data.fileName
-//        val ctx = binding.root.context
-//
-//        data.linkToFile?.let {
-//            Glide.with(ctx)
-//                .load(it)
-//                .into(binding.documentImage)
-//        }
     }
 
     override fun getLayoutId(): Int = R.layout.document_item
