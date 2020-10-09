627f649fb5d3d8b34333e18b80146263e5171f0a
Anton Kogan <akogan777@gmail.com>
2020-10-09 11:46:52 +0300

    task info, parser extension, need to add some data to task entity.

--- app/src/main/java/com/abona_erp/driver/app/ui/taskInfo/TaskInfoFragment.kt
+++ app/src/main/java/com/abona_erp/driver/app/ui/taskInfo/TaskInfoFragment.kt
@@ -51,9 +51,82 @@ class TaskInfoFragment : Fragment() {
     private fun addTestRows(task: TaskEntity) {
         val jsonObject = JSONObject(Gson().toJson(task).trim())
         val map: HashMap<String, String> = java.util.HashMap()
-        JsonParser.parseJson(jsonObject, map)
-
         val linearContent: LinearLayout = taskInfoFragmentBinding.infoContainer
+        /*
+
+        /*
+{
+  "actionType": "1",
+  "activityIds": [
+    15,
+    16,
+    17
+  ],
+  "address": {
+    "City": "Kharkiv",
+    "Latitude": 0,
+    "Longitude": 0,
+    "Name1": "medical goods",
+    "Nation": "UA",
+    "Street": "Test4 str",
+    "ZIP": "11500"
+  },
+  "changeReason": 0,
+  "confirmationType": "RECEIVED",
+  "constacts": [
+    {
+      "ContactType": 0,
+      "Number": "004915114744063",
+      "NumberType": 0
+    }
+  ],
+  "dangerousGoods": {
+    "ADRClass": 0,
+    "DangerousGoodsClassType": 0,
+    "IsGoodsDangerous": true,
+    "UNNo": ""
+  },
+  "kundenName": "ANTON KOGAN TEST4",
+  "mandantId": 3,
+  "orderDetails": {
+    "CustomerName": "ANTON KOGAN TEST4",
+    "CustomerNo": 60238,
+    "OrderNo": 202040165,
+    "ReferenceIdCustomer1": "4321"
+  },
+  "palletExchange": {
+    "ExchangeType": 0,
+    "IsDPL": false,
+    "PalletsAmount": -1
+  },
+  "status": "0",
+  "taskDueDateFinish": "2020-10-06T00:00:00",
+  "taskDueDateStart": "0001-01-01T00:00:00",
+  "taskId": 9961
+}         */
+         */
+        val include= arrayOf(
+            "address",
+            "Longitude",
+            "Latitude",
+            "ReferenceId2",
+            "LoadingOrder",
+            "DangerousGoods",
+            "ADRClass",
+            "UNNo",
+            "palletExchange",
+            "ExchangeType",
+            "PalletsAmount",
+            "IsDPL",
+            "constacts",
+            "Name",
+            "Number",
+            "orderDetails",
+            "ReferenceIdCustomer1",
+            "Notes"
+        )
+        JsonParser.parseJson(jsonObject, map, include)
+
 
         map.entries.forEach { entry ->
             run {
--- app/src/main/java/com/abona_erp/driver/app/ui/utils/JsonParser.kt
+++ app/src/main/java/com/abona_erp/driver/app/ui/utils/JsonParser.kt
@@ -5,8 +5,11 @@ import org.json.JSONArray
 import org.json.JSONException
 import org.json.JSONObject
 
+/**
+ * Created by Anton Kogan on 10/9/2020
+ * akogan777@gmail.com
+ */
 object JsonParser {
-
     val TAG = "JsonParser"
     /**
      * parse json object
@@ -16,10 +19,22 @@ object JsonParser {
      */
     @Throws(JSONException::class)
     fun parseJson(objJson: Any?, map :HashMap<String, String>): Map<String, String> {
+        return parseJson(objJson, map, null)
+    }
+
+    /**
+     * parse json object
+     * @param objJson
+     * @param include - all  keys, that you want to display
+     * @return  Map<String, String>
+     * @throws JSONException
+     */
+    @Throws(JSONException::class)
+    fun parseJson(objJson: Any?, map :HashMap<String, String>, include : Array<String>?): Map<String, String> {
         // If obj is a json array
         if (objJson is JSONArray) {
             for (i in 0 until objJson.length()) {
-                parseJson(objJson[i], map)
+                parseJson(objJson[i], map, include)
             }
         } else if (objJson is JSONObject) {
             val it: Iterator<*> = objJson.keys()
@@ -30,18 +45,21 @@ object JsonParser {
                     is JSONArray -> {
                         Log.e(TAG, " JSONArray: $jobject")
                         parseJson(
-                            jobject, map
+                            jobject, map, include
                         )
                     }
                     is JSONObject -> {
                         Log.e(TAG, " JSONObject: $jobject")
                         parseJson(
-                            jobject, map
+                            jobject, map, include
                         )
                     }
                     else -> {
-//                        Log.e(TAG, " adding to map: $key $jobject")
-                        map[key] = jobject.toString()
+                        if(include == null || include.contains(key)) // here is check for include param
+                        {
+                            map[key] = jobject.toString()
+                          //  Log.e(TAG, " adding to map: $key $jobject")
+                        }
                     }
                 }
             }
