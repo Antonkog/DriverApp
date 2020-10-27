1c89558f326f5baa0e57c890d6e8240074147dfd
Anton Kogan <a.kogan777@gmail.com>
2020-10-27 10:32:16 +0200

    set firebase firebase crashlytics.

--- app/src/main/java/com/abona_erp/driverapp/App.kt
+++ app/src/main/java/com/abona_erp/driverapp/App.kt
@@ -2,6 +2,7 @@ package com.abona_erp.driverapp
 
 import android.app.Application
 import com.google.firebase.FirebaseApp
+import com.google.firebase.crashlytics.FirebaseCrashlytics
 import dagger.hilt.android.HiltAndroidApp
 
 @HiltAndroidApp
@@ -15,7 +16,11 @@ class App : Application() {
     override fun onCreate() {
         super.onCreate()
 
-        FirebaseApp.initializeApp(this);
+        FirebaseApp.initializeApp(this)
+
+        if (!BuildConfig.DEBUG) {
+            FirebaseCrashlytics.getInstance().setCrashlyticsCollectionEnabled(true)
+        }
 
     }
 }
