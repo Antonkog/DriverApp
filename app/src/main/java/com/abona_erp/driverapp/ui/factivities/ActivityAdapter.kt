44d944deb3536a17654206294a395b093c09174a
Anton Kogan <akogan777@gmail.com>
2020-11-02 18:04:46 +0200

    UI: Set activity screen (Task#8671)

--- app/src/main/java/com/abona_erp/driverapp/ui/factivities/ActivityAdapter.kt
+++ app/src/main/java/com/abona_erp/driverapp/ui/factivities/ActivityAdapter.kt
@@ -20,35 +20,40 @@ class ActivityAdapter(itemClickListener: DriverActFragment) :
 
     override fun bindData(data: ActivityWrapper, binding: ActivityItemBinding) {
 
-        val jsonObject = JSONObject(Gson().toJson(data).trim())
-        val map: HashMap<String, String> = java.util.HashMap()
-        JsonParser.parseJson(jsonObject, map)
+        binding.textActName.text = data.activity.name
 
-        val linearContent: LinearLayout = binding.linlayDescription
-        if (linearContent.childCount > 0) linearContent.removeAllViews()
-        map.entries.forEach { entry ->
-            run {
-                val row = LayoutInflater.from(binding.root.context)
-                    .inflate(R.layout.task_row, null, false)
-                row.findViewById<TextView>(R.id.txt_item_row).text = entry.key + " " + entry.value
-                linearContent.addView(row)
-            }
-        }
 
         if(data.buttonVisible) binding.buttonActConfirm.visibility = View.VISIBLE
         else binding.buttonActConfirm.visibility = View.GONE
 
         when (data.activity.activityStatus) {
-            ActivityStatus.PENDING -> binding.buttonActConfirm.text =
-                binding.root.resources.getString(R.string.activity_start)
-            ActivityStatus.RUNNING -> binding.buttonActConfirm.text =
-                binding.root.resources.getString(R.string.activity_next)
-            ActivityStatus.FINISHED -> binding.buttonActConfirm.visibility = View.GONE
-            ActivityStatus.ENUM_ERROR -> binding.buttonActConfirm.visibility = View.GONE
+            ActivityStatus.PENDING -> {
+                binding.textFinished.visibility = View.INVISIBLE
+                binding.buttonActConfirm.text =
+                    binding.root.resources.getString(R.string.activity_start)
+            }
+            ActivityStatus.RUNNING -> {
+                binding.textFinished.visibility = View.INVISIBLE
+                binding.buttonActConfirm.text =
+                    binding.root.resources.getString(R.string.activity_next)
+            }
+            ActivityStatus.FINISHED -> {
+                binding.buttonActConfirm.visibility = View.GONE
+                binding.textFinishedValue.text = data.activity.finished
+            }
+            ActivityStatus.ENUM_ERROR -> {
+                binding.textFinished.visibility = View.INVISIBLE
+                binding.buttonActConfirm.visibility = View.GONE
+            }
         }
 
-        if(data.isLastActivity) binding.buttonActConfirm.text =
-            binding.root.resources.getString(R.string.activity_finish)
+        if(data.isLastActivity){
+            binding.buttonActConfirm.text =
+                binding.root.resources.getString(R.string.activity_finish)
+        }
+
+
+        binding.textStartedValue.text = data.activity.started
 
         binding.buttonActConfirm.setOnClickListener {
             itemClickListener?.onLazyItemClick(data)
